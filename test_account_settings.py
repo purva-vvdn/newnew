2153f1fb133066f1c9e6cf28f13d1a5c48fee860
root <root@30987544d208>
2025-02-11 12:18:38 +0530

    Pushed Successfully

---
+++ test_account_settings.py
@@ -0,0 +1,7 @@
+class TestEdit:
+def test_to_verify_the_two_step_verification(self):
+        edit_driver = Edit(self.driver)
+        edit_driver.open_login_page()
+        edit_driver.enter_username('unique56@yopmail.com')
+        edit_driver.enter_password('Sak@1421')
+        edit_driver.clicking_on_sign_in()
